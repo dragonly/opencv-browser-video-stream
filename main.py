5f9905b9bbffae83dc1e53af3ba37c1196480a06
dragonly <liyilongko@gmail.com>
2018-11-24 00:17:55 +0800

    disable cache, adequate quality but much fewer bytes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from flask import Flask, make_response
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 cap = cv2.VideoCapture(0)
@@ -12,7 +12,8 @@ def index():
 @app.route('/capture')
 def capture():
     ret, frame = cap.read()
-    ret, buffer = cv2.imencode('.jpg', frame)
+    ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
     resp = make_response(buffer.tobytes())
     resp.headers['Content-Type'] = 'image/jpeg'
+    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     return resp
